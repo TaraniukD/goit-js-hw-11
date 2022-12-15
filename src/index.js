720d962242cf53fa4778824328062ebeef09fbcd
Dmytro <109168390+TaraniukD@users.noreply.github.com>
2022-12-15 19:04:58 +0200

    task11

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import SimpleLightbox from "simplelightbox";
 // Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from "simplelightbox";
+
+const BASE_URL = 'https://pixabay.com/api';
+const myKey = '32102465-275d51e71b27b4572d9937886';
+
+function fetchImg() {
+    return fetch(`${BASE_URL}/?key=${myKey}&q=yellow+flowers&image_type=photo&orientation=horizontal&safesearch=true`).then(res => res.json());
+}
+
+fetchImg(cat).then(res => res.json(
+    console.log(res);
+));
+
+console.log(21)
