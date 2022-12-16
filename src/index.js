3bc1459a7b9d593ac826d4e5c6bf83521894ed07
Dmytro <109168390+TaraniukD@users.noreply.github.com>
2022-12-16 19:43:09 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,90 +6,149 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 
 const BASE_URL = 'https://pixabay.com/api';
 const API_KEY = '32102465-275d51e71b27b4572d9937886';
+const pageSize = 40;
+let currentPage = 1;
 
 const refs = {
     formEl: document.getElementById('search-form'),
     inputEl: document.querySelector('#search-form input'),
-    hitsContainer: document.querySelector('.gallery')
+    hitsContainer: document.querySelector('.gallery'),
+    loadMoreBtn: document.querySelector('.load-more')
 };
 
+console.log(refs.loadMoreBtn)
 const API = axios.create({
     baseURL: BASE_URL,
 });
 
 refs.formEl.addEventListener('submit', onFormSubmit);
 
+
 function onFormSubmit(e) {
     e.preventDefault();
 
-    fetchImg({
-        query: refs.inputEl.value
-    })
+    fetchImg(refs.inputEl.value).then(({hits}) => {
+        const hitsMoreEl = createMoreHitsEl(hits);
+        refs.hitsContainer.insertAdjacentHTML('beforeend', hitsMoreEl)
+            });
 }
 
-function fetchImg({query}) {
-    const urlAPI = `${BASE_URL}/?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`;
-    axios.get (urlAPI)
+refs.loadMoreBtn.addEventListener('click', e => {
+    if (currentPage > totalPages) return
+    currentPage = Number(currentPage) + 1;
+
+    fetchImg(refs.inputEl.value).then(({hits}) => {
+const hitsMoreEl = createMoreHitsEl(hits);
+refs.hitsContainer.insertAdjacentHTML('beforeend', hitsMoreEl)
+    });
+})
+
+
+function calculatePagination(totalHits) {
+    totalPages = Math.floor(totalHits / pageSize)
+    // createPagesElements ()
+  }
+
+// function createPagesElements () {
+//     let pagesElements = '';
+    
+//     for (let i = 1; i <= totalPages; i += 1 ) {
+//       pagesElements += `<li class="page-item"><a class="page-link" href="#" data-page=${i}>${i}</a></li>`
+//     }
+  
+//     return pagesElements
+//   }
+
+function fetchImg(query) {
+    const urlAPI = `${BASE_URL}/?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${pageSize}&page=${currentPage}`;
+   return axios.get(urlAPI)
 
   .then(res => res.data)
-  .then(({hits}) => {
+  .then(({hits, totalHits}) => {
         if (hits.length === 0) {
             Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.'); 
         }
-     renderHTML(hits)
+    //  renderHTML(hits)
+    calculatePagination(totalHits)
+     return {hits, totalHits}
         })
-     .catch(function (error) {
-
-        })
+     .catch(error => console.log(error))
    }
 
-function renderHTML(hits) {
-refs.hitsContainer.innerHTML = '';
-const hitsEl = hits.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
-    return `
-    <div class="photo-card">
-    <a class="gallery__item" href="${largeImageURL}">
-      <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-      </a>
-      <div class="info">
-        <p class="info-item">
-          <b>Likes: <br/><span class="info-span">${likes}</span></b>
-        </p>
-        <p class="info-item">
-          <b>Views: <br/><span class="info-span">${views}</span></b>
-        </p>
-        <p class="info-item">
-          <b>Comments: <br/><span class="info-span">${comments}</span></b>
-        </p>
-        <p class="info-item">
-          <b>Downloads: <br/><span class="info-span">${downloads}</span></b>
-        </p>
-      </div>
-    </div>`
-}).join('');
 
-refs.hitsContainer.insertAdjacentHTML('beforeend', hitsEl)
 
-let gallery = new SimpleLightbox('.gallery a', { 
-    captions: true,
-    captionType:'attr',
-    captionsData: "alt",
-    captionPosition:'bottom',
-    captionDelay: 250,
-    
- });
-
-gallery.refresh()
-
-gallery.on('show.simplelightbox', function () {
-	// do something…
-});
-
-gallery.on('error.simplelightbox', function (e) {
-	console.log(e); // some usefull information
-});
-
+function createMoreHitsEl (hits) {
+    return hits.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+        return `
+        <div class="photo-card">
+        <a class="gallery__item" href="${largeImageURL}">
+          <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+          </a>
+          <div class="info">
+            <p class="info-item">
+              <b>Likes: <br/><span class="info-span">${likes}</span></b>
+            </p>
+            <p class="info-item">
+              <b>Views: <br/><span class="info-span">${views}</span></b>
+            </p>
+            <p class="info-item">
+              <b>Comments: <br/><span class="info-span">${comments}</span></b>
+            </p>
+            <p class="info-item">
+              <b>Downloads: <br/><span class="info-span">${downloads}</span></b>
+            </p>
+          </div>
+        </div>`
+    }).join(''); 
 }
+
+// function renderHTML() {
+// refs.hitsContainer.innerHTML = '';
+// const hitsEl = hits.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+//     return `
+//     <div class="photo-card">
+//     <a class="gallery__item" href="${largeImageURL}">
+//       <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+//       </a>
+//       <div class="info">
+//         <p class="info-item">
+//           <b>Likes: <br/><span class="info-span">${likes}</span></b>
+//         </p>
+//         <p class="info-item">
+//           <b>Views: <br/><span class="info-span">${views}</span></b>
+//         </p>
+//         <p class="info-item">
+//           <b>Comments: <br/><span class="info-span">${comments}</span></b>
+//         </p>
+//         <p class="info-item">
+//           <b>Downloads: <br/><span class="info-span">${downloads}</span></b>
+//         </p>
+//       </div>
+//     </div>`
+// }).join('');
+
+// refs.hitsContainer.insertAdjacentHTML('beforeend', hitsEl)
+
+// let gallery = new SimpleLightbox('.gallery a', { 
+//     captions: true,
+//     captionType:'attr',
+//     captionsData: "alt",
+//     captionPosition:'bottom',
+//     captionDelay: 250,
+    
+//  });
+
+// gallery.refresh()
+
+// gallery.on('show.simplelightbox', function () {
+// 	// do something…
+// });
+
+// gallery.on('error.simplelightbox', function (e) {
+// 	console.log(e); // some usefull information
+// });
+
+// }
 
 
 
