ada42ef6e71734e8ebd8c58bdd0f25dbd97c7488
Dmytro <109168390+TaraniukD@users.noreply.github.com>
2022-12-17 12:00:59 +0200

    task 11

--- src/index.js
+++ src/index.js
@@ -6,8 +6,11 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 
 const BASE_URL = 'https://pixabay.com/api';
 const API_KEY = '32102465-275d51e71b27b4572d9937886';
-const pageSize = 40;
+let pageSize = 40;
 let currentPage = 1;
+let totalPages = undefined;
+
+// console.log(totalPages)
 
 const refs = {
     formEl: document.getElementById('search-form'),
@@ -16,38 +19,148 @@ const refs = {
     loadMoreBtn: document.querySelector('.load-more')
 };
 
-console.log(refs.loadMoreBtn)
 const API = axios.create({
     baseURL: BASE_URL,
 });
 
-refs.formEl.addEventListener('submit', onFormSubmit);
+refs.loadMoreBtn.classList.add('hiden')
+refs.formEl.addEventListener('submit', onFormImgSubmit);
+refs.loadMoreBtn.addEventListener('click', loadMoreImg );
 
 
-function onFormSubmit(e) {
+
+function onFormImgSubmit(e) {
     e.preventDefault();
-
-    fetchImg(refs.inputEl.value).then(({hits}) => {
+    refs.loadMoreBtn.classList.remove('hiden')
+    
+    getImgbyAxios(refs.inputEl.value.trim()).then(({hits, totalHits}) => {
         const hitsMoreEl = createMoreHitsEl(hits);
         refs.hitsContainer.insertAdjacentHTML('beforeend', hitsMoreEl)
+
+        Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+        simplelightboxGallery();
             });
 }
 
-refs.loadMoreBtn.addEventListener('click', e => {
-    if (currentPage > totalPages) return
+function loadMoreImg() {
+  
+  refs.loadMoreBtn.classList.remove('hiden')
+
+ console.log(totalPages)
+
+  if (currentPage === totalPages) {
+    refs.loadMoreBtn.classList.add('hiden');
+    Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
+  return;
+  }
     currentPage = Number(currentPage) + 1;
 
-    fetchImg(refs.inputEl.value).then(({hits}) => {
-const hitsMoreEl = createMoreHitsEl(hits);
-refs.hitsContainer.insertAdjacentHTML('beforeend', hitsMoreEl)
+    getImgbyAxios(refs.inputEl.value).then(({hits, totalHits}) => {
+  const hitsMoreEl = createMoreHitsEl(hits);
+  refs.hitsContainer.insertAdjacentHTML('beforeend', hitsMoreEl);
+  calculatePagination(totalHits);
+  console.log(totalPages)
+  
+  simplelightboxGallery();
     });
-})
+}
 
+function getImgbyAxios(query) {
+  const urlAPI = `${BASE_URL}/?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${pageSize}&page=${currentPage}`;
+  
+  return axios.get(urlAPI)
+
+.then(res => res.data)
+.then(({hits, totalHits}) => {
+      if (hits.length === 0) {
+          Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.'); 
+      } else {
+        // Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+        return {hits, totalHits}
+      }
+      })
+
+.catch(error => console.log(error))
+}
+
+function createMoreHitsEl (hits) {
+  clearGelleryContainer()
+
+  return hits.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+      return `
+      <div class="photo-card">
+      <a class="gallery__item" href="${largeImageURL}">
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+        </a>
+        <div class="info">
+          <p class="info-item">
+            <b>Likes: <br/><span class="info-span">${likes}</span></b>
+          </p>
+          <p class="info-item">
+            <b>Views: <br/><span class="info-span">${views}</span></b>
+          </p>
+          <p class="info-item">
+            <b>Comments: <br/><span class="info-span">${comments}</span></b>
+          </p>
+          <p class="info-item">
+            <b>Downloads: <br/><span class="info-span">${downloads}</span></b>
+          </p>
+        </div>
+      </div>`
+  }).join(''); 
+
+}
+
+function clearGelleryContainer() {
+  refs.hitsContainer.innerHTML = '';
+}
 
 function calculatePagination(totalHits) {
-    totalPages = Math.floor(totalHits / pageSize)
-    // createPagesElements ()
-  }
+   return totalPages = Math.floor(totalHits / pageSize)
+}
+
+function simplelightboxGallery() {
+  let gallery = new SimpleLightbox('.gallery a', { 
+    captions: true,
+    captionType:'attr',
+    captionsData: "alt",
+    captionPosition:'bottom',
+    captionDelay: 250,
+    
+ });
+
+gallery.refresh()
+
+gallery.on('show.simplelightbox', function () {
+	// do something…
+});
+
+gallery.on('error.simplelightbox', function (e) {
+	console.log(e); // some usefull information
+});
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // function createPagesElements () {
 //     let pagesElements = '';
@@ -59,48 +172,7 @@ function calculatePagination(totalHits) {
 //     return pagesElements
 //   }
 
-function fetchImg(query) {
-    const urlAPI = `${BASE_URL}/?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${pageSize}&page=${currentPage}`;
-   return axios.get(urlAPI)
 
-  .then(res => res.data)
-  .then(({hits, totalHits}) => {
-        if (hits.length === 0) {
-            Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.'); 
-        }
-    //  renderHTML(hits)
-    calculatePagination(totalHits)
-     return {hits, totalHits}
-        })
-     .catch(error => console.log(error))
-   }
-
-
-
-function createMoreHitsEl (hits) {
-    return hits.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
-        return `
-        <div class="photo-card">
-        <a class="gallery__item" href="${largeImageURL}">
-          <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-          </a>
-          <div class="info">
-            <p class="info-item">
-              <b>Likes: <br/><span class="info-span">${likes}</span></b>
-            </p>
-            <p class="info-item">
-              <b>Views: <br/><span class="info-span">${views}</span></b>
-            </p>
-            <p class="info-item">
-              <b>Comments: <br/><span class="info-span">${comments}</span></b>
-            </p>
-            <p class="info-item">
-              <b>Downloads: <br/><span class="info-span">${downloads}</span></b>
-            </p>
-          </div>
-        </div>`
-    }).join(''); 
-}
 
 // function renderHTML() {
 // refs.hitsContainer.innerHTML = '';
